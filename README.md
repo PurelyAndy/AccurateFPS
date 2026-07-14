62cfb6d242a5a2130628a9205aac0a2f0bde286b
PurelyAndy <72871450+PurelyAndy@users.noreply.github.com>
2026-07-14 18:40:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AccurateFPS
+makes the max fps slider in settings snap to 30, 36, 48, 60, 72, 90, 120, 144, 165, and 240 rather than multiples of 10.
---
+++ src/main/java/sh/lemming/accuratefps/mixin/OptionsMixin.java
@@ -0,0 +1,61 @@
+package sh.lemming.accuratefps.mixin;
+import net.minecraft.client.Options;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import java.util.function.IntFunction;
+import java.util.function.ToIntFunction;
+
+@Mixin(Options.class)
+public class OptionsMixin {
+    private static final int SNAP_RANGE = 2; // on either side, so 5 total values will snap to 1 value
+    private static int[] snaps = new int[] {
+            30, 36, 48, 60, 72, 90, 120, 144, 165, 240
+    };
+
+    @ModifyArgs(
+        method = "<init>",
+        at = @At(
+            value = "INVOKE",
+            target = "net/minecraft/client/OptionInstance$IntRange.xmap (Ljava/util/function/IntFunction;Ljava/util/function/ToIntFunction;)Lnet/minecraft/client/OptionInstance$SliderableValueSet;",
+            ordinal = 0
+        ),
+        slice = @Slice(
+            from = @At(value = "CONSTANT", args = "intValue=26"),
+            to = @At(value = "CONSTANT", args = "intValue=260")
+        )
+    )
+    private void customSnappingLogic(Args args) {
+        IntFunction<Integer> sliderProgressToValue = (progress) -> {
+            for (int snap : snaps) {
+                if (Math.abs(progress - snap) <= SNAP_RANGE) {
+                    return snap;
+                }
+            }
+            return progress;
+        };
+        ToIntFunction<Integer> valueToSliderProgress = value -> value;
+
+        args.setAll(sliderProgressToValue, valueToSliderProgress);
+    }
+
+    @ModifyArgs(
+        method = "<init>",
+        at = @At(
+            value = "INVOKE",
+            target = "net/minecraft/client/OptionInstance$IntRange.<init> (II)V",
+            ordinal = 0
+        ),
+        slice = @Slice(
+            from = @At(value = "CONSTANT", args = "intValue=26"),
+            to = @At(value = "CONSTANT", args = "intValue=260")
+        )
+    )
+    private void fixedRange(Args args) {
+        args.setAll(10, 260);
+    }
+}
